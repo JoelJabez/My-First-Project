c1bccbd452189b917658f2e556862b886053f107
joel0135 <joeldavethu@gmail.com>
2023-06-24 16:33:19 +0800

    Jetbrains Academy, My First Project(Part 1/3)

---
+++ My First Project/task/calculator.py
@@ -0,0 +1,10 @@
+def main():
+    print("Prices: ")
+    print("Bubblegum: $2")
+    print("Toffee: $0.2")
+    print("Ice cream: $5")
+    print("Milk chocolate: $4")
+    print("Doughnut: $2.5")
+    print("Pancake: $3.2")
+
+main()
